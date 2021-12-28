ef053bf5123e1e90d175a5f8c1bfea5541368681
Datawheel <ryan@datawheel.us>
2021-12-28 16:15:24 -0600

    improved title function as it deals with cuts on levels not included in drilldowns

--- src/toolbox/title.js
+++ src/toolbox/title.js
@@ -1,8 +1,39 @@
+import { AggregatorType } from "@datawheel/olap-client";
 import { findTimeRange } from "./find";
 import { getColumnId } from "./strings";
 
 /**
+ * Human-readable names of special aggregator types.
+ * Needed to make certain types of measures more explicit about their measure's derivation.
+ * 
+ * If an aggregation type needs clarification beyond measure name, insert it here.
+ */
+const SPECIAL_AGGREGATOR_QUALIFIERS = {
+  [AggregatorType.AVG]: "Average",
+  [AggregatorType.MAX]: "Max",
+  [AggregatorType.MIN]: "Min"
+};
+
+/**
  * Returns a common title string from a list of parameters.
+ * ---------------------------------------------------------
+ * Examples of title variations:
+ * 
+ * Standard drilldowns with no cuts:
+ * _MEASURE_NAME by DRILLDOWN_1 and DRILLDOWN_2_
+ * 
+ * Special aggregation type (e.g. average):
+ * _AGGREGATOR_TYPE_NAME MEASURE_NAME by DRILLDOWN_
+ * 
+ * Chart with single drilldown of type "time" used to filter data using "timeline" feature:
+ * _MEASURE_NAME Over Time_
+ * 
+ * Chart with time drilldown plotted on axis:
+ * _MEASURE_NAME by DRILLDOWN (TIME_LEVEL_NAME: CURRENT_PERIOD)_
+ * 
+ * Chart with cuts applied _on levels not included in drilldowns/levels_: 
+ * _MEASURE_NAME of Selected CUT_LEVEL_NAME Members by DRILLDOWN_1_
+ * 
  * @param {VizBldr.Struct.Chart} chart
  * @param {object} options
  */
@@ -11,49 +42,51 @@ export function chartTitleGenerator(chart, options) {
   const {dg, measureSet} = chart;
   const {members} = dg;
 
-  const measureName = measureSet.measure.name;
+  let title;
 
-  const cuts = [];
+  // FIRST, add measure (with appropriate qualifiers) to the start of the title
+  title = `${getAggregationTypeQualifier(measureSet.measure.aggregatorType)}${measureSet.measure.name}`;
 
-  const allLevels = [...chart.levels];
-  const allLevelNames = allLevels.map(obj => obj.name);
-  const allCutNames = [...dg.cuts.keys()];
+  /** Set of cut level names, to be filtered to include only levels not accounted for in drilldowns */
+  const allCutNames = new Set(dg.cuts.keys());
+  /** List of Level names to include in CUT clause */
+  const cutLabels = [];
+  /** Labels of drilldowns with only a single member */
+  const singleMemberDrilldownLabels = [];
 
-  let n = allLevels.length;
-  while (n--) {
-    const level = allLevels[n];
-    const levelName = level.caption;
-    const values = members[levelName];
+  /** List of Dimension names to be shown as  */
+  const drilldownNames = chart.levels
+    .map(lvl => lvl.caption)
+    .filter(levelName => {
+      const values = members[levelName];
+      // if there is only one dimension of a drilldown dimension...
+      if (values.length === 1) {
+        // don't show it as a selected cut
+        if (allCutNames.has(levelName)) allCutNames.delete(levelName);
+        // but show it as a separate label
+        const label = `${levelName}: ${values[0]}`;
+        singleMemberDrilldownLabels.unshift(label);
+        return false;
+      }
+      else if (allCutNames.has(levelName)) allCutNames.delete(levelName);
+      return true;
+    });
 
-    let label;
-    // if level has one member, remove from level list and add as cut
-    if (values.length === 1) {
-      label = `${levelName}: ${values[0]}`;
-      const levelIndex = allLevelNames.indexOf(levelName);
-      if (levelIndex > -1) allLevelNames.splice(levelIndex, 1);
-      const cutIndex = allCutNames.indexOf(levelName);
-      if (levelIndex > -1) allCutNames.splice(cutIndex, 1);
-      cuts.unshift(label);
-    }
-  }
+  // if time drilldown exists but it is not included in Chart.levels but there is a cut on it, add to cut level list
+  if (dg.timeDrilldown?.caption && allCutNames.has(dg.timeDrilldown.caption)) allCutNames.delete(dg.timeDrilldown.caption);
+  cutLabels.unshift(...allCutNames.values());
 
-  allCutNames.forEach(cutLvlName => {
-    const numMembers = dg.cuts.get(cutLvlName)?.length;
-    if (!allLevelNames.includes(cutLvlName)) {
-      let label = `${cutLvlName}: ${numMembers} Selected`
-      cuts.unshift(label);
-    }
-  });
+  // add labels for cut levels not included in Chart.levels
+  if (cutLabels.length > 0) title += ` of Selected ${arrayToSentence(cutLabels)} Members`;
 
-  let title = measureName;
+  // add labels for levels with single member
+  if (singleMemberDrilldownLabels.length > 0) title += ` (${singleMemberDrilldownLabels.join(", ")})`;
 
-  if (cuts.length > 0) title += ` (${arrayToSentence(cuts)})`;
-
-  // add levels / dimensions to titles
-  if (allLevelNames.length > 0) {
+  // add levels with multiple members to titles
+  if (drilldownNames.length > 0) {
     title += chart.isTopTen
-      ? ` for top ${arrayToSentence(allLevelNames)}`
-      : ` by ${arrayToSentence(allLevelNames)}`;
+      ? ` for top ${arrayToSentence(drilldownNames)}`
+      : ` by ${arrayToSentence(drilldownNames)}`;
   }
 
   let titleFn = null;
@@ -76,10 +109,6 @@ export function chartTitleGenerator(chart, options) {
     }
     // else, if time is shown on one axis, say "Over Time"
     else title += ` Over Time`;
-  }
-
-  if (title[title.length - 1] === ",") {
-    return title.slice(0, -1)
   }
 
   return titleFn || title;
@@ -105,6 +134,17 @@ function arrayToSentence(strings, options = {}) {
     return [bulk.join(allWords), last].join(lastWord);
   }
   return strings.join("");
+}
+
+/**
+ * Returns a string giving a few qualifying words (if necessary) to add to a measure in the case
+ * that a measure does not have a self-evident aggregation method (like sum)
+ * @param {import("@datawheel/olap-client").AggregatorType} aggregationType - 
+ * @returns 
+ */
+function getAggregationTypeQualifier(aggregationType) {
+  const qualifier = SPECIAL_AGGREGATOR_QUALIFIERS[aggregationType];
+  return qualifier ? `${qualifier} ` : "";
 }
 
 /**
