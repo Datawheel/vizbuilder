8e49861085f72cecd441d770a6787e2dcbb45022
Francisco Abarzua <git.id@frabarz.cl>
2024-02-06 13:07:06 -0300

    moves backend URL to dev env config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,29 @@
 import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 
+/**
+ * Replace the target URL of the backend server in this file
+ * to make vite proxy the requests and avoid CORS issues.
+ */
+const url = new URL("https://api.datasaudi.datawheel.us/tesseract/");
+const headers = {};
+
+// ensure the target URL ends with a trailing slash
+url.pathname = `${url.pathname}/`.replace(/\/{2,}/g, "/");
+
 export default defineConfig({
   plugins: [react()],
   root: "./example/",
   server: {
     proxy: {
-      "/tesseract": {
+      "/tesseract/": {
         // auth: "username:password",
-        target: "https://dev.sebrae.api.datawheel.us/tesseract/",
         changeOrigin: true,
+        followRedirects: true,
+        headers,
+        rewrite: path => path.replace(/^\/tesseract\//, url.pathname),
         secure: false,
-        rewrite: path => path.replace(/^\/tesseract/, "")
+        target: url.origin
       }
     }
   }
