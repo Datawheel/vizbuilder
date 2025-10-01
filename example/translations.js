2650aa1976c52c227eb50d02a5032a91bb2fd604
Felipe Ramos <framosmorale@student.unimelb.edu.au>
2025-10-01 17:37:15 -0300

    update en translation

--- example/translations.js
+++ example/translations.js
@@ -2,7 +2,58 @@ import { defaultTranslation } from "../dist/react.esm.js";
 
 /** @type {Record<string, import("../dist/react").Translation>} */
 export const translations = {
-  en: defaultTranslation,
+  en: {
+        action_close: "Close",
+        action_enlarge: "Enlarge",
+        action_share: "Share",
+        share_copied: "Link copied",
+        action_fileissue: "Report an issue",
+        action_retry: "Retry",
+        aggregator: {
+            average: "Average {{measure}}",
+            max: "Max {{measure}}",
+            min: "Min {{measure}}",
+            sum: "{{measure}}"
+        },
+        chart_labels: {
+            ci: "Confidence Interval",
+            moe: "Margin of Error",
+            source: "Source",
+            collection: "Collection"
+        },
+        error: {
+            "detail": "",
+            "message": "Details: \"{{message}}\".",
+            "title": "Error"
+        },
+        list: {
+            join: ", ",
+            suffix: "{{rest}}, and {{item}}",
+            prefix: "{{list}}",
+            n_more: "{{n}} more"
+        },
+        title: {
+            main_on_period: "{{values}} by {{series}} in {{time_period}}",
+            main_over_period: "{{values}} by {{series}}, {{time_scale}}",
+            main: "{{values}} by {{series}}",
+            measure_on_period: "{{values}} on {{time_period}}",
+            measure_over_period: "{{values}}, {{time_scale}}",
+            nonidealstate: "No results",
+            series_members: "{{series}}",
+            series: "{{series}}",
+            time_range: "in {{from}}-{{to}}",
+            total: "Total: {{value}}",
+            scale_year: "Annualy",
+            scale_quarter: "Quarterly",
+            scale_month: "Monthly"
+        },
+        transient: {
+            title_one_row: "The dataset has only one row and can't be used to generate charts.",
+            title_loading: "Generating charts...",
+            title_empty: "No results",
+            description_empty: "The selected combination of parameters can't be used to generate a meaningful set of charts. Try changing some parameters (maybe applying some restriction in a column) and generating charts again."
+        }    
+  },  
   ar: {
     action_close: "إغلاق",
     action_enlarge: "تكبير",
